59ed0b6f4d18ee1439b10d386489df18886bfa37
Maxim Fol <max.folko@gmail.com>
2024-07-02 01:20:30 +0300

    Build 3158.4215: Update config settings
    Updated the Terraform and YOLO5 configuration settings for performance improvements.
    
    - Changed 'desired_capacity', 'min_size', and 'max_size' parameters in the Terraform main.tf file from 1 and 2 to 0. This change was essential to remove unnecessary resource allocations and free more resources.
    - In the yolo5 .env file, updated 'YOLO5_IMG_NAME' from 'magrufol/yolo5:4.0.2' to 'magrufol/yolo5:4.0.3' to use the latest image version, improving performance and stability of our application.

--- terraform/main.tf
+++ terraform/main.tf
@@ -348,11 +348,11 @@ module "autoscaling_group" {
   launch_template_id = aws_launch_template.max-aws-asg.id
   launch_template_version = var.asg_launch_version
   #1
-  desired_capacity = 1
+  desired_capacity = 0
   #1
-  min_size = 1
+  min_size = 0
   #2
-  max_size = 2
+  max_size = 0
   vpc_zone_identifier = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 
   tags = {
